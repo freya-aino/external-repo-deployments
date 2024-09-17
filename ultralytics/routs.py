fdb34dbab51a8002fcaaf46110b6b06ef67df071
kristina-aino <kristina.ai@proton.me>
2024-09-18 00:47:02 +0200

    reworked ultralytics deployment

--- ultralytics/routs.py
+++ ultralytics/routs.py
@@ -1,244 +1,202 @@
 import cv2
 import numpy as np
+import torch as T
 
 from torch import tensor, float32
 from torchvision.transforms import Resize
 from fastapi import UploadFile, FastAPI, HTTPException, File
+from fastapi.responses import JSONResponse
 from typing import Union
 from ultralytics import YOLO
 
+# ----------------------------------------------------------------
 
-async def file_to_image(file: Union[UploadFile, bytes], out_size=(640, 640)):
+device = "cuda" if T.cuda.is_available() else "cpu"
+
+detection_model = None
+pose_model = None
+classification_model = None
+segmentation_model = None
+obb_model = None
+
+app = FastAPI()
+
+# ----------------------------------------------------------------
+
+async def file_to_image(file: UploadFile, out_size=(640, 640)):
     
     # load image from buffer
-    if isinstance(file, UploadFile):
-        contents = await file.read()
-    elif isinstance(file, bytes):
-        contents = file
-    else:
-        raise HTTPException(status_code=400, detail="Invalid file format!")
+    contents = await file.read()
     
     image = np.frombuffer(contents, np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    image = tensor(image, dtype=float32).cuda() / 255
+    image = tensor(image, dtype=float32).to(device)
     image = image.permute(2, 0, 1).unsqueeze(0)
-    image = Resize(out_size)(image)
+    image = Resize(out_size)(image) / 255.0
+    
+    image = T.clamp(image, 0.0, 1.0)
     
     return image
 
-# MODELS -----------------------------------------------------
-
-detectionCOCO_model = None
-poseCOCO_model = None
-classificationImageNet_model = None
-detectionOpenImagev7_model = None
-segmentationCOCO_model = None
-obbDOTAv1_model = None
-
-app = FastAPI()
-
+# ----------------------------------------------------------------
 
 @app.on_event("startup")
 def load_models():
-    global detectionCOCO_model
-    global poseCOCO_model
-    global classificationImageNet_model
+    global detection_model
+    global pose_model
+    global classification_model
     global detectionOpenImagev7_model
-    global segmentationCOCO_model
-    global obbDOTAv1_model
+    global segmentation_model
+    global obb_model
     
-    obbDOTAv1_model = YOLO("./OBBDOTAv1.pt").cuda()
-    detectionCOCO_model = YOLO("./DetectionCOCO.pt").cuda()
-    poseCOCO_model = YOLO("./PoseCOCO.pt").cuda()
-    classificationImageNet_model = YOLO("./ClassificationImageNet.pt").cuda()
-    detectionOpenImagev7_model = YOLO("./DetectionOpenImagev7.pt").cuda()
-    segmentationCOCO_model = YOLO("./SegmentationCOCO.pt").cuda()
+    detection_model = YOLO("yolov8x.pt").to(device)
+    obb_model = YOLO("yolov8x-obb.pt").to(device)
+    pose_model = YOLO("yolov8x-pose-p6.pt").to(device)
+    classification_model = YOLO("yolov8x-cls.pt").to(device)
+    segmentation_model = YOLO("yolov8x-seg.pt").to(device)
 
+# ----------------------------------------------------------------
 
-@app.post("/detectionCOCO")
-async def detectionCOCO(file: Union[UploadFile, bytes] = File(...)):
+@app.post("/detection")
+async def detection(file: UploadFile = File(...)):
     
-    assert detectionCOCO_model is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = detectionCOCO_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
+    try:
+        assert detection_model is not None, "Model not loaded"
+        image_tensor = await file_to_image(file)
+        ret = detection_model.predict(image_tensor)
         
-        if ret[i] is None:
-            continue
+        results = []
+        for i in range(len(ret)):
+            
+            if ret[i] is None or ret[i].boxes is None or len(ret[i].boxes) == 0:
+                continue
+            
+            results.append(
+                {
+                    "bboxes": [{
+                        "bbox": box.xywhn.cpu().numpy().tolist()[0],
+                        "confidence": box.conf.cpu().numpy().tolist()[0],
+                        "class": ret[i].names[box.cls.cpu().numpy().tolist()[0]]
+                    } for box in ret[i].boxes]
+                }
+            )
         
-        results.append(
-            {
-                "classes": ret[i].names,
+        return JSONResponse(content={"result": results}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
+
+@app.post("/pose")
+async def pose(file: UploadFile = File(...)):
+    
+    try:
+        assert pose_model is not None, "Model not loaded"
+        image_tensor = await file_to_image(file)
+        ret = pose_model.predict(image_tensor)
+        
+        results = []
+        for i in range(len(ret)):
+            
+            if ret[i] is None or ret[i].boxes is None or ret[i].keypoints is None or len(ret[i].boxes) == 0 or len(ret[i].keypoints) == 0:
+                continue
+            
+            results.append({
                 "bboxes": [{
-                    "bbox": box.xywhn.cpu().numpy().tolist()[0],
-                    "confidence": box.conf.cpu().numpy().tolist()[0],
-                    "class": int(box.cls.cpu().numpy().tolist()[0])
-                } for box in ret[i].boxes]
-            }
-        )
-    
-    return {
-        "status": "success",
-        "result": results
-    }
+                        "bbox": box.xywhn.cpu().numpy().tolist()[0],
+                        "confidence": box.conf.cpu().numpy().tolist()[0],
+                    } for box in ret[i].boxes],
+                "landmarks": [{
+                        "keypoints": lan.xyn.cpu().numpy().tolist(),
+                        "confidence": lan.conf.cpu().numpy().tolist()
+                    } for lan in ret[i].keypoints]
+            })
+            
+        return JSONResponse(content={"result": results}, status_code=200)
+        
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
 
+@app.post("/classification")
+async def classification(file: UploadFile = File(...)):
+    
+    try:
+        assert classification_model  is not None, "Model not loaded"
+        image_tensor = await file_to_image(file)
+        ret = classification_model.predict(image_tensor)
+        
+        results = []
+        for i in range(len(ret)):
+            
+            if ret[i] is None or ret[i].probs is None:
+                continue
+            
+            results.append({
+                "Top5": list(map(lambda a: ret[i].names[a], ret[i].probs.top5)),
+                "top5conf": ret[i].probs.top5conf.cpu().numpy().tolist()
+            })
+        
+        return JSONResponse(content={"result": results}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
 
-@app.post("/poseCOCO")
-async def poseCOCO(file: Union[UploadFile, bytes] = File(...)):
+@app.post("/segmentation")
+async def segmentation(file: UploadFile = File(...)):
     
-    assert poseCOCO_model is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = poseCOCO_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
+    try:
+        assert segmentation_model is not None, "Model not loaded"
+        image_tensor = await file_to_image(file)
+        ret = segmentation_model.predict(image_tensor)
         
-        if ret[i] is None:
-            continue
+        results = []
+        for i in range(len(ret)):
+            
+            if ret[i] is None or ret[i].masks is None or ret[i].masks is None:
+                continue
+            
+            results.append({
+                "masks": [{
+                    "class": ret[i].names[bbox.cls.cpu().numpy().tolist()[0]],
+                    "confidence": bbox.conf.cpu().numpy().tolist()[0],
+                    "bbox": bbox.xywhn.cpu().numpy().tolist()[0],
+                    "mask": mask.xyn[0].tolist()
+                } for (bbox, mask) in zip(ret[i].boxes, ret[i].masks)
+            ]})
         
-        results.append({
-            "bboxes": [{
-                    "bbox": box.xywhn.cpu().numpy().tolist()[0],
-                    "confidence": box.conf.cpu().numpy().tolist()[0],
-                    "class": int(box.cls.cpu().numpy().tolist()[0])
-                } for box in ret[i].boxes],
-            "landmarks": [{
-                    "keypoints": lan.xyn.cpu().numpy().tolist(),
-                    "confidence": lan.conf.cpu().numpy().tolist()
-                } for lan in ret[i].keypoints]
-        })
-    
-    return {
-        "status": "success",
-        "result": results
-    }
+        return JSONResponse(content={"result": results}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
 
-@app.post("/claaificationImageNet")
-async def claaificationImageNet(file: Union[UploadFile, bytes] = File(...)):
+@app.post("/obb")
+async def obb(file: UploadFile = File(...)):
     
-    assert classificationImageNet_model  is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = classificationImageNet_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
+    try:
+        assert obb_model is not None, "Model not loaded"
+        image_tensor = await file_to_image(file)
+        ret = obb_model.predict(image_tensor)
         
-        if ret[i] is None:
-            continue
-        
-        results.append({
-            "classes": ret[i].names,
-            "probability": ret[i].probs.data.cpu().numpy().tolist(),
-            "Top5": map(int, ret[i].probs.top5)
-        })
-    
-    return {
-        "status": "success",
-        "result": results
-    }
-
-@app.post("/detectionOpenImagev7")
-async def detectionOpenImagev7(file: Union[UploadFile, bytes] = File(...)):
-    
-    assert detectionOpenImagev7_model is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = detectionOpenImagev7_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
-        
-        if ret[i] is None:
-            continue
-        
-        results.append({
-            "classes": ret[i].names,
-            "bboxes": [{
-                "bbox": box.xywhn.cpu().numpy().tolist()[0],
-                "confidence": box.conf.cpu().numpy().tolist()[0],
-                "class": int(box.cls.cpu().numpy().tolist()[0])
-            } for box in ret[i].boxes],
-        })
-    
-    return {
-        "status": "success",
-        "result": results
-    }
-
-@app.post("/segmentationCOCO")
-async def segmentationCOCO(file: Union[UploadFile, bytes] = File(...)):
-    
-    assert segmentationCOCO_model is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = segmentationCOCO_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
-        
-        if ret[i] is None:
-            continue
-        
-        results.append({
-            "classes": ret[i].names,
-            "masks": [{
-                "class": int(bbox.cls.cpu().numpy().tolist()[0]),
-                "confidence": bbox.conf.cpu().numpy().tolist()[0],
-                "bbox": bbox.xywhn.cpu().numpy().tolist()[0],
-                "mask": mask.xyn[0].tolist()
-            } for (bbox, mask) in zip(ret[i].boxes, ret[i].masks)
-        ]})
-    
-    return {
-        "status": "success",
-        "result": results
-    }
-
-@app.post("/obbDOTAv1")
-async def obbDOTAv1(file: Union[UploadFile, bytes] = File(...)):
-    
-    assert obbDOTAv1_model is not None, "Model not loaded"
-    
-    image_tensor = await file_to_image(file)
-    
-    ret = obbDOTAv1_model.predict(image_tensor)
-    
-    results = []
-    for i in range(len(ret)):
-        
-        if ret[i] is None:
-            continue
-        
-        bboxes = [obb.xywhr.cpu().numpy().tolist()[0] for obb in ret[i].obb]
-        
-        results.append({
-            "classes": ret[i].names,
-            "bboxes": [{
-                "class": int(obb.cls.cpu().numpy().tolist()[0]),
-                "confidence": obb.conf.cpu().numpy().tolist()[0],
-                "bbox": [
-                    bboxes[j][0] / image_tensor.shape[2], 
-                    bboxes[j][1] / image_tensor.shape[3], 
-                    bboxes[j][2] / image_tensor.shape[2], 
-                    bboxes[j][3] / image_tensor.shape[3], 
-                    bboxes[j][4]
-                ]
-            } for (j, obb) in enumerate(ret[i].obb)
-        ]})
-    
-    return {
-        "status": "success",
-        "result": results
-    }
+        results = []
+        for i in range(len(ret)):
+            
+            if ret[i] is None or ret[i].obb is None:
+                continue
+            
+            bboxes = [obb.xywhr.cpu().numpy().tolist()[0] for obb in ret[i].obb]
+            
+            results.append({
+                "bboxes": [{
+                    "class": ret[i].names[obb.cls.cpu().numpy().tolist()[0]],
+                    "confidence": obb.conf.cpu().numpy().tolist()[0],
+                    "bbox": [
+                        bboxes[j][0] / image_tensor.shape[2], 
+                        bboxes[j][1] / image_tensor.shape[3], 
+                        bboxes[j][2] / image_tensor.shape[2], 
+                        bboxes[j][3] / image_tensor.shape[3], 
+                        bboxes[j][4]
+                    ]
+                } for (j, obb) in enumerate(ret[i].obb)
+            ]})
+            
+        return JSONResponse(content={"result": results}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
